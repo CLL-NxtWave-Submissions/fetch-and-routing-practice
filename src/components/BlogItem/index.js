4c89820103eb08cc2aa6bb574a0210ce8bd50c4a
Surya T N <suryatej.nim@yahoo.com>
2022-10-06 08:09:06 +0000

    Implemented JSX elements to render the intended UI with BlogItem component

--- src/components/BlogItem/index.js
+++ src/components/BlogItem/index.js
@@ -1,1 +1,25 @@
-// Write your JS code here
+import {Link} from 'react-router-dom'
+import './index.css'
+
+const BlogItem = props => {
+  const {itemData} = props
+  const {id, title, imageUrl, avatarUrl, author, topic} = itemData
+
+  return (
+    <Link to={`/blogs/${id}`}>
+      <li className="blog-item-container">
+        <img className="blog-item-image" src={imageUrl} alt={title} />
+        <div className="blog-item-content-container">
+          <p className="blog-item-topic">{topic}</p>
+          <h1 className="blog-item-title">{title}</h1>
+          <div className="blog-author-details-container">
+            <img className="blog-item-avatar" src={avatarUrl} alt={author} />
+            <p className="blog-item-author">{author}</p>
+          </div>
+        </div>
+      </li>
+    </Link>
+  )
+}
+
+export default BlogItem
