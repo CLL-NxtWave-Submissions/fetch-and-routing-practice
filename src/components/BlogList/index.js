4dc1ba82b2fa00cda0a09a3722544385c0b3ba2f
Surya T N <suryatej.nim@yahoo.com>
2022-10-06 06:59:34 +0000

    Implemented BlogList class component with CSS styling and JSX elements to render the intended UI

--- src/components/BlogList/index.js
+++ src/components/BlogList/index.js
@@ -1,1 +1,32 @@
-// Write your JS code here
+import {Component} from 'react'
+import BlogItem from '../BlogItem'
+import './index.css'
+
+const blogsApiUrl = 'https://apis.ccbp.in/blogs'
+
+export default class BlogList extends Component {
+  state = {
+    blogList: [],
+  }
+
+  async componentDidMount() {
+    const blogsDataAPIResponse = await fetch(blogsApiUrl)
+    const blogsData = await blogsDataAPIResponse
+
+    this.setState({
+      blogList: blogsData,
+    })
+  }
+
+  render() {
+    const {blogList} = this.state
+
+    return (
+      <ul className="blog-list-container">
+        {blogList.map(blogListItem => (
+          <BlogItem itemData={blogListItem} />
+        ))}
+      </ul>
+    )
+  }
+}
